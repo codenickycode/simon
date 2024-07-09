dd0bfe39ab3b5a327aaad8d5012664ddf82e2402
Nick Carbone <drumnickydrum@gmail.com>
2024-07-09 12:59:27 -0400

    root lint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: { es2020: true },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  ignorePatterns: ["dist", ".eslintrc.js"],
+  parser: "@typescript-eslint/parser",
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-unused-expressions": [
+      "error",
+      { allowShortCircuit: true },
+    ],
+  },
+  settings: {
+    "max-warnings": 0,
+  },
+};
--- worker/.eslintrc.cjs
+++ worker/.eslintrc.cjs
@@ -1,7 +1,3 @@
 module.exports = {
-	root: true,
-	env: { es2020: true },
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-	ignorePatterns: ['dist', '.eslintrc.cjs'],
-	parser: '@typescript-eslint/parser',
+	extends: ['../.eslintrc.js'],
 };
