6cff38fb56360fe0c91f1ac0509cffc07f467088
Nick Carbone <drumnickydrum@gmail.com>
2024-07-14 12:12:21 -0400

    add key listener hook to separate out of pad-controller

--- client/src/components/Gamepad/Gamepad.tsx
+++ client/src/components/Gamepad/Gamepad.tsx
@@ -1,55 +1,53 @@
 import { ActivePads, PadId } from "./types";
 import { Pad } from "./Pad";
-import { isPadActive } from "../../utils/pads";
+import { isPadActive, padKeyToPadId } from "../../utils/pads";
+import { useEffect } from "react";
 
 interface GamepadProps {
   activePads: ActivePads;
-  onPadDown: (padId: PadId) => void;
-  onPadUp: (padId: PadId) => void;
+  onUserPadDown: (padId: PadId) => void;
+  onUserPadUp: (padId: PadId) => void;
   isComputerTurn: boolean;
   isUserTurn: boolean;
 }
 
 export const Gamepad = (props: GamepadProps) => {
-  /** We allow pointer down while disabled and the machine will check if they
-   * can "jump start" their turn */
-  const onDisabledPointerDown = props.onPadDown;
+  useKeyListeners({
+    onUserPadDown: props.onUserPadDown,
+    onUserPadUp: props.onUserPadUp,
+  });
   return (
     <div className="w-full aspect-square rounded-full bg-black flex items-center justify-center">
       <div className="relative w-80 grid grid-cols-2">
         <Pad
           padId="green"
           active={isPadActive("green", props.activePads)}
-          onPointerDown={() => props.onPadDown("green")}
-          onDisabledPointerDown={() => onDisabledPointerDown("green")}
-          onPointerUp={() => props.onPadUp("green")}
+          onPointerDown={() => props.onUserPadDown("green")}
+          onPointerUp={() => props.onUserPadUp("green")}
           disabled={!props.isUserTurn}
           className="justify-self-end self-end pad-color-green rounded-tl-full"
         />
         <Pad
           padId="red"
           active={isPadActive("red", props.activePads)}
-          onPointerDown={() => props.onPadDown("red")}
-          onDisabledPointerDown={() => onDisabledPointerDown("red")}
-          onPointerUp={() => props.onPadUp("red")}
+          onPointerDown={() => props.onUserPadDown("red")}
+          onPointerUp={() => props.onUserPadUp("red")}
           disabled={!props.isUserTurn}
           className="self-end pad-color-red rounded-tr-full"
         />
         <Pad
           padId="yellow"
           active={isPadActive("yellow", props.activePads)}
-          onPointerDown={() => props.onPadDown("yellow")}
-          onDisabledPointerDown={() => onDisabledPointerDown("yellow")}
-          onPointerUp={() => props.onPadUp("yellow")}
+          onPointerDown={() => props.onUserPadDown("yellow")}
+          onPointerUp={() => props.onUserPadUp("yellow")}
           disabled={!props.isUserTurn}
           className="justify-self-end pad-color-yellow rounded-bl-full"
         />
         <Pad
           padId="blue"
           active={isPadActive("blue", props.activePads)}
-          onPointerDown={() => props.onPadDown("blue")}
-          onDisabledPointerDown={() => onDisabledPointerDown("blue")}
-          onPointerUp={() => props.onPadUp("blue")}
+          onPointerDown={() => props.onUserPadDown("blue")}
+          onPointerUp={() => props.onUserPadUp("blue")}
           disabled={!props.isUserTurn}
           className="pad-color-blue rounded-br-full"
         />
@@ -58,4 +56,44 @@ export const Gamepad = (props: GamepadProps) => {
       </div>
     </div>
   );
+};
+
+const useKeyListeners = ({
+  onUserPadDown,
+  onUserPadUp,
+}: {
+  onUserPadDown: (padId: PadId) => void;
+  onUserPadUp: (padId: PadId) => void;
+}) => {
+  useEffect(() => {
+    const onKeyDown = (event: KeyboardEvent) => {
+      if (event.repeat) {
+        event.preventDefault();
+        return;
+      }
+      const padId = padKeyToPadId(event.key);
+      if (!padId) {
+        return;
+      }
+      onUserPadDown(padId);
+    };
+    window.addEventListener("keydown", onKeyDown);
+    return () => {
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [onUserPadDown]);
+
+  useEffect(() => {
+    const onKeyUp = (event: KeyboardEvent) => {
+      const padId = padKeyToPadId(event.key);
+      if (!padId) {
+        return;
+      }
+      onUserPadUp(padId);
+    };
+    window.addEventListener("keyup", onKeyUp);
+    return () => {
+      window.removeEventListener("keyup", onKeyUp);
+    };
+  }, [onUserPadUp]);
 };
