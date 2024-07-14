3ad899417f1e9878777a3d9e14a9c0b3c2215c99
Nick Carbone <drumnickydrum@gmail.com>
2024-07-14 10:42:17 -0400

    rm console log

--- client/src/components/Gamepad/Pad.tsx
+++ client/src/components/Gamepad/Pad.tsx
@@ -24,7 +24,6 @@ export const Pad = (props: PadProps) => {
       props.onPointerDown();
     }
   };
-  console.log(padColorClass);
   return (
     <button
       onPointerDown={handlePointerDown}
