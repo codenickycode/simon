ef08eaa9cbf04fcb144d1e2d37d5bcdfda0e2c31
Nick Carbone <drumnickydrum@gmail.com>
2024-07-21 19:15:12 -0400

    start a modal with deets

--- client/src/components/HighScore.tsx
+++ client/src/components/HighScore.tsx
@@ -1,14 +1,41 @@
+import { useCallback, useState } from "react";
 import { useHighScoreApi } from "../services/api.high-score";
+import { Modal } from "./ui/Modal";
 import { Spinner } from "./ui/Spinner";
 
 export const HighScore = () => {
   const { query } = useHighScoreApi();
+  const [modalOpen, setModalOpen] = useState(false);
+  const openModal = useCallback(() => setModalOpen(true), []);
+  const closeModal = useCallback(() => setModalOpen(false), []);
   return (
-    <div className="font-bold text-center flex items-center">
-      <p className="md:text-lg">
+    <div className="flex items-center">
+      <button
+        onClick={openModal}
+        className="md:text-lg font-bold text-center "
+        disabled={query.isFetching || query.isError}
+      >
         High Score:<span className="mx-1"></span>
-        <Spinner isSpinning={query.isFetching}>{query.data?.score}</Spinner>
-      </p>
+        <Spinner isSpinning={query.isFetching}>
+          {query.data?.score ?? "?"}
+        </Spinner>
+      </button>
+      <Modal isOpen={modalOpen} onClose={closeModal}>
+        <div>
+          <p className="md:text-lg ">
+            <span>High Score:</span>
+            <span className="mx-1">{query.data?.score}</span>
+          </p>
+          <p className="md:text-lg ">
+            <span>User:</span>
+            <span className="mx-1">{query.data?.name}</span>
+          </p>
+          <p className="md:text-lg ">
+            <span>On:</span>
+            <span className="mx-1">{query.data?.date}</span>
+          </p>
+        </div>
+      </Modal>
     </div>
   );
 };
