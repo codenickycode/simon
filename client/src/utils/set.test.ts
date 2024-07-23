947b9c7a92d1997a52f37bdc610a51af286a4fca
Nick Carbone <drumnickydrum@gmail.com>
2024-07-23 09:16:40 -0400

    set test

--- client/src/utils/set.test.ts
+++ client/src/utils/set.test.ts
@@ -1,3 +1,16 @@
+import { toggleSetItem } from "./set";
+
 describe("toggleSetItem", () => {
-  test.todo("it works");
+  it("can add an item immutably", () => {
+    const original = new Set([1, 2, 3]);
+    const result = toggleSetItem({ set: original, item: 4, op: "add" });
+    expect([...result]).toEqual([1, 2, 3, 4]);
+    expect([...original]).toEqual([1, 2, 3]);
+  });
+  it("can remove an item immutably", () => {
+    const original = new Set([1, 2, 3]);
+    const result = toggleSetItem({ set: original, item: 3, op: "delete" });
+    expect([...result]).toEqual([1, 2]);
+    expect([...original]).toEqual([1, 2, 3]);
+  });
 });
