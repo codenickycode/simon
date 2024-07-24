1b1f5d7f913883bfe79a672531309c062e493fff
Nick Carbone <drumnickydrum@gmail.com>
2024-07-24 08:51:13 -0400

    test toggle set

--- client/src/utils/set.test.ts
+++ client/src/utils/set.test.ts
@@ -1,4 +1,5 @@
-import { toggleSetItem } from "./set";
+import { toggleSetItem, useToggleItems } from "./set";
+import { renderHook } from "@testing-library/react";
 
 describe("toggleSetItem", () => {
   it("can add an item immutably", () => {
@@ -12,5 +13,38 @@ describe("toggleSetItem", () => {
     const result = toggleSetItem({ set: original, item: 3, op: "delete" });
     expect([...result]).toEqual([1, 2]);
     expect([...original]).toEqual([1, 2, 3]);
+  });
+});
+
+describe("useToggleItems", () => {
+  it("initializes an empty set when no init given", () => {
+    const result = renderHook(() => useToggleItems()).result.current;
+    expect([...result.items]).toEqual([]);
+  });
+  it("initializes a provided set when given", () => {
+    const result = renderHook(() => useToggleItems(new Set([1, 2, 3]))).result
+      .current;
+    expect([...result.items]).toEqual([1, 2, 3]);
+  });
+  it("toggles items on", () => {
+    const rendered = renderHook(() => useToggleItems(new Set([1, 2, 3])));
+    rendered.result.current.on(4);
+    rendered.result.current.on(5);
+    rendered.rerender();
+    expect([...rendered.result.current.items]).toEqual([1, 2, 3, 4, 5]);
+  });
+  it("toggles items off", () => {
+    const rendered = renderHook(() => useToggleItems(new Set([1, 2, 3])));
+    rendered.result.current.off(3);
+    rendered.result.current.off(2);
+    rendered.rerender();
+    expect([...rendered.result.current.items]).toEqual([1]);
+  });
+  it("ignores already on/off", () => {
+    const rendered = renderHook(() => useToggleItems(new Set([1, 2, 3])));
+    rendered.result.current.on(1);
+    rendered.result.current.off(4);
+    rendered.rerender();
+    expect([...rendered.result.current.items]).toEqual([1, 2, 3]);
   });
 });
