5f6bdffd31f9a60ef6a922483d030d4ab1caef8a
Nick Carbone <drumnickydrum@gmail.com>
2024-07-27 08:58:51 -0400

    add type import rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,11 @@ module.exports = {
   root: true,
   env: { es2020: true },
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/consistent-type-exports": "error",
+    "@typescript-eslint/consistent-type-imports": "error",
+  },
   ignorePatterns: ["dist", ".eslintrc.js"],
   parser: "@typescript-eslint/parser",
   rules: {
