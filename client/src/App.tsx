07847322d87816c4d7764f9e4574315663d168e4
Nick Carbone <drumnickydrum@gmail.com>
2024-07-27 10:07:18 -0400

    try again

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import { GameOverModal } from "./components/game-over/game-over-modal";
 import { Gamepad } from "./components/gamepad/gamepad";
 import { PadId, usePadController } from "./components/pad-controller";
 
-// @ts-expect-error asdffff
+// @ts-expect-error asdfffff
 const pad: PadId = 1;
 
 const queryClient = new QueryClient();
