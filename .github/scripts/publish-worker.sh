9803f4100b53d6570c915f3b514ec9381ea61aec
Nick Carbone <drumnickydrum@gmail.com>
2024-07-31 07:50:29 -0400

    worker script

--- .github/scripts/publish-worker.sh
+++ .github/scripts/publish-worker.sh
@@ -2,20 +2,20 @@
 
 set -e
 
-# if [[ "$GITHUB_REF_NAME" != "main" && "$GITHUB_REF_NAME" != "dev" ]]; then
-#     echo "Not publishing: this branch is not main or dev."
-#     exit 0
-# fi
+if [[ "$GITHUB_REF_NAME" != "main" && "$GITHUB_REF_NAME" != "dev" ]]; then
+    echo "Not publishing: this branch is not main or dev."
+    exit 0
+fi
 
 if [[ "$GITHUB_REF_NAME" == "dev" ]]; then
     # Deploy to preview for dev branch
-    pnpm wrangler pages deploy ./client/dist \
+    pnpm wrangler deploy ./worker/dist \
     --project-name simon \
     --branch "dev" \
     --commit-hash "$GITHUB_SHA" \
     --commit-message "dev deployment"
 else
     # Deploy to production for main branch
-    pnpm wrangler pages deploy ./client/dist \
+    pnpm wrangler deploy ./worker/dist \
     --project-name simon
 fi
