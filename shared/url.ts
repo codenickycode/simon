d7c7efe93bfb228c9d301ff00a2f55ba7ce41ff5
Nick Carbone <drumnickydrum@gmail.com>
2024-07-30 21:21:14 -0400

    fix prod worker url

--- shared/url.ts
+++ shared/url.ts
@@ -3,5 +3,5 @@ export const WORKER_PATH_HIGH_SCORE = '/high-score';
 export const getWorkerUrl = (isDev: boolean) => {
   return isDev
     ? 'http://localhost:8787'
-    : 'https://https://simon.codenickycode.workers.dev';
+    : 'https://simon.codenickycode.workers.dev';
 };
