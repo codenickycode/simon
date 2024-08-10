16678fc4e515d491fc4e6409920f83cf7b6c677f
Nick Carbone <drumnickydrum@gmail.com>
2024-08-10 11:54:33 -0400

    send sourcemaps to sentry

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,23 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import type { PluginOption } from 'vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({ plugins: [react() as PluginOption[]] });
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  console.log({ env });
+  return {
+    plugins: [
+      react() as PluginOption[],
+      sentryVitePlugin({
+        org: 'codenickycode',
+        project: 'simon-client',
+        authToken: env.SENTRY_AUTH_TOKEN,
+      }),
+    ],
+    build: {
+      sourcemap: true,
+    },
+  };
+});
