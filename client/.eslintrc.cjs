419298b8c10113e87f0d61d019a1acb6a1745246
Nick Carbone <drumnickydrum@gmail.com>
2024-08-16 08:13:29 -0400

    prevent import of tone (#78)

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,11 +1,32 @@
 module.exports = {
-  extends: ["../.eslintrc.cjs", "plugin:react-hooks/recommended"],
+  extends: ['../.eslintrc.cjs', 'plugin:react-hooks/recommended'],
   env: { browser: true },
-  plugins: ["react-refresh"],
+  plugins: ['react-refresh'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: 'tone',
+            message:
+              'tone should only be imported from modules within the "services" directory',
+          },
+        ],
+      },
+    ],
   },
+  overrides: [
+    // allow tone only within "services" directory
+    {
+      files: ['**/services/**'],
+      rules: {
+        'no-restricted-imports': ['off', { paths: [{ name: 'tone' }] }],
+      },
+    },
+  ],
 };
