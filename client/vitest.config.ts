1e6578d5a88b62f61ea0f7088e2c7f4c77e0e7ca
Nick Carbone <drumnickydrum@gmail.com>
2024-08-16 13:21:58 -0400

    fix vitest config

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig, mergeConfig } from 'vitest/config';
 import viteConfig from './vite.config';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/test/setup.ts',
-    },
-  }),
+export default defineConfig((configEnv) =>
+  mergeConfig(
+    viteConfig(configEnv),
+    defineConfig({
+      test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/test/setup.ts',
+      },
+    }),
+  ),
 );
