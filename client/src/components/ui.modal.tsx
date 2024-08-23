e736f457bc97025e5193ba54a0672ff39d5121ad
Nick Carbone <drumnickydrum@gmail.com>
2024-08-23 06:06:30 -0400

    modal close

--- client/src/components/ui.modal.tsx
+++ client/src/components/ui.modal.tsx
@@ -67,7 +67,7 @@ export const Modal = ({
         onClick={(e) => e.stopPropagation()}
       >
         <button
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+          className="absolute top-2 right-2 text-gray-500 hover:text-gray-300 text-3xl"
           onClick={onClose}
         >
           &times;
