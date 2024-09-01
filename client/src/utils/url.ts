2179c84a57bb9c0bb0a30229f6be9c0fb737ad2e
Nick Carbone <drumnickydrum@gmail.com>
2024-09-01 16:46:24 -0400

    Dev worker (#103)
    * try dev env
    
    * fetch from dev worker
    
    * better matcher

--- client/src/utils/url.ts
+++ client/src/utils/url.ts
@@ -1,5 +1,10 @@
 export const getServerUrl = () => {
-  return import.meta.env.DEV
-    ? 'http://localhost:8787'
-    : 'https://simon.codenickycode.workers.dev';
+  if (import.meta.env.DEV) {
+    return 'http://localhost:8787';
+  }
+  let subdomain = 'simon';
+  if (window.location.hostname.includes('.pages.dev')) {
+    subdomain += '-dev';
+  }
+  return `https://${subdomain}.codenickycode.workers.dev`;
 };
