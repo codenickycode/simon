66c8babf1d64d45f3158199f983a1b27019a0233
Nick Carbone <drumnickydrum@gmail.com>
2024-09-14 14:04:19 -0400

    rm console.log

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,8 +6,6 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  console.log(env.E2E);
-  console.log(process.env.E2E);
   return {
     plugins: [
       react() as PluginOption[],
